867ba5cb2838e535a8bcd95a5e85730b1e3fe01c
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-02 01:17:58 -0600

    test: update factorybot

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
     name { Faker::Games::Minecraft.item }
-    description { Faker::Food.description }
+    description { Faker::HitchhikersGuideToTheGalaxy.quote }
     unit_price { Faker::Number.between(from: 10000, to: 100000) }
     association :merchant
   end
