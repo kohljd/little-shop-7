c2227a3ac0460135a271e189ac47bd8a4e18f353
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-03 00:54:57 -0600

    feat: update merchant's bulk discount

--- spec/features/merchants/bulk_discounts/edit_spec.rb
+++ spec/features/merchants/bulk_discounts/edit_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe "Merchant Bulk Discount Edit", type: :feature do
     let!(:bulk_discount_1) {merchant_1.bulk_discounts.create!(discount: 20, quantity: 10)}
 
     before do
-      visit edit_merchant_bulk_discount(merchant_1, bulk_discount_1)
+      visit edit_merchant_bulk_discount_path(merchant_1, bulk_discount_1)
     end
 
     it "displays update form with bulk discount's current info" do
-      expect(page).to have_field("Discount", value: 20)
-      expect(page).to have_field("Quantity", value: 10)
+      expect(page).to have_field("Discount", with: 20)
+      expect(page).to have_field("Quantity", with: 10)
     end
 
     it "updated form redirects to Bulk Discount Show" do
@@ -24,12 +24,13 @@ RSpec.describe "Merchant Bulk Discount Edit", type: :feature do
       expect(page).to have_content("Quantity Threshold: 30 items")
 
       click_on "Update Discount"
-      expect(page).to have_field("Discount", value: 25)
-      expect(page).to have_field("Quantity", value: 30)
+      expect(page).to have_field("Discount", with: 25)
+      expect(page).to have_field("Quantity", with: 30)
 
       fill_in "Quantity", with: 22
       click_on "Submit"
       expect(current_path).to eq(merchant_bulk_discount_path(merchant_1, bulk_discount_1))
+      save_and_open_page
       expect(page).to have_content("Quantity Threshold: 22 items")
     end
 
@@ -37,10 +38,12 @@ RSpec.describe "Merchant Bulk Discount Edit", type: :feature do
       fill_in "Discount", with: ''
       fill_in "Quantity", with: ''
       click_on "Submit"
-
-      expect(current_path).to eq(edit_merchant_bulk_discount(merchant_1, bulk_discount_1))
+      
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(merchant_1, bulk_discount_1))
       expect(page).to have_content("Discount can't be blank")
+      expect(page).to have_content("Discount is not a number")
       expect(page).to have_content("Quantity can't be blank")
+      expect(page).to have_content("Quantity is not a number")
     end
   end
 end
