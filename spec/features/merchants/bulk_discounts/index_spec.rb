d404dc87565c78335f22b963bdf5d9dc3aa465bb
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-02 22:58:40 -0600

    test: only bulk discounts for that merchant listed

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -3,11 +3,21 @@ require "rails_helper"
 RSpec.describe "Merchant's Bulk Discounts Index", type: :feature do
   describe "when merchant visits" do
     let!(:merchant_1) {create(:merchant)}
+    let!(:merchant_2) {create(:merchant)}
     let!(:bulk_discount_1) {merchant_1.bulk_discounts.create!(discount: 20, quantity: 10)}
     let!(:bulk_discount_2) {merchant_1.bulk_discounts.create!(discount: 15, quantity: 15)}
+    let!(:bulk_discount_3) {merchant_2.bulk_discounts.create!(discount: 15, quantity: 15)}
 
     before do
         visit merchant_bulk_discounts_path(merchant_1)
+    end
+
+    describe "displays bulk discounts" do
+      it "only displays discount's for that merchant" do
+        expect(page).to have_css("#bulk_discount-#{bulk_discount_1.id}")
+        expect(page).to have_css("#bulk_discount-#{bulk_discount_2.id}")
+        expect(page).to_not have_css("#bulk_discount-#{bulk_discount_3.id}")
+      end
     end
 
     describe "displays bulk discounts w/their attributes" do
