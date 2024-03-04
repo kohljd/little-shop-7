e528521c082b78d79916e35bf8656390225c7a49
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-04 15:42:28 -0600

    feat: discount must be >= 100

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -2,11 +2,12 @@ require "rails_helper"
 
 RSpec.describe BulkDiscount, type: :model do
   describe "validations" do
-    it { should validate_presence_of :discount}
+    it { should validate_presence_of :discount }
     it { should validate_numericality_of(:discount).is_greater_than(0) }
+    it { should validate_numericality_of(:discount).is_less_than_or_equal_to(100) }
 
     it { should validate_presence_of :quantity}
-    it { should validate_numericality_of(:quantity).is_greater_than(0)}
+    it { should validate_numericality_of(:quantity).is_greater_than(0) }
   end
 
   describe "relationships" do
