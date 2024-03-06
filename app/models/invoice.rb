eebad3bf860782167730ffa2b54d7a436d12e5f8
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-06 01:10:53 -0600

    feat: entire invoice's discounted revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -29,6 +29,36 @@ class Invoice < ApplicationRecord
       .sum("invoice_items.quantity * invoice_items.unit_price")
   end
 
+  def entire_invoice_discount_info
+    InvoiceItem.find_by_sql(
+      "SELECT SUM(invoice_items.quantity * invoice_items.unit_price) AS undiscounted_revenue,
+        CAST(SUM((1 - ((bulk_discounts.discount + 0.0) / 100)) * (invoice_items.quantity * invoice_items.unit_price)) AS INTEGER) AS discounted_revenue,
+        CAST(SUM(((bulk_discounts.discount + 0.0)/100) * invoice_items.quantity * invoice_items.unit_price) AS INTEGER) AS amount_customer_saved,
+        bulk_discounts.id AS applied_discount
+
+      FROM invoice_items
+      JOIN items ON items.id = invoice_items.item_id
+      JOIN merchants ON merchants.id = items.merchant_id
+      JOIN bulk_discounts ON bulk_discounts.merchant_id = merchants.id
+
+      WHERE invoice_items.invoice_id = #{self.id}
+
+      AND invoice_items.quantity >= bulk_discounts.quantity
+      AND bulk_discounts.discount = (
+        SELECT MAX(discount) FROM bulk_discounts as inner_discounts
+        WHERE invoice_items.quantity >= inner_discounts.quantity
+        AND bulk_discounts.merchant_id = inner_discounts.merchant_id
+        GROUP BY inner_discounts.discount
+        ORDER BY inner_discounts.discount DESC LIMIT 1
+      )
+      GROUP BY invoice_items.item_id, bulk_discounts.id"
+    )
+  end
+
+  def total_invoice_discounted_revenue
+    total_revenue - entire_invoice_discount_info.map {|invoice_item| invoice_item.amount_customer_saved}.sum
+  end
+
   def discount_info(merchant)
     InvoiceItem.find_by_sql(
       "SELECT SUM(invoice_items.quantity * invoice_items.unit_price) AS undiscounted_revenue,
